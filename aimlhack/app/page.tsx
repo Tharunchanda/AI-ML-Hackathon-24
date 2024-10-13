1c5fd5f649311598945616d48ce1d224dd3a23ad
Vinamra Garg <gargvinamra21@gmail.com>
2024-10-13 11:30:52 +0530

    file structure

---
+++ aimlhack/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+    <div>
+      AI ML HACKATHON ,2024
+      <Link href="/ps1">Problem Statement 1</Link>
+      <Link href="/ps2">Problem Statement 2</Link>
+    </div>
+  );
+}
